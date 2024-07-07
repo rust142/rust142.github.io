7fe8b59e260eb5f89afbdc0b99c198db02ffce81
Agung Maulana <amdev142@gmail.com>
2024-07-07 18:28:39 +0700

    feat: update navigation

--- src/components/navigation-menu/Navigation.vue
+++ src/components/navigation-menu/Navigation.vue
@@ -4,7 +4,7 @@ import { defineComponent } from 'vue';
 import NavigationMenu from './NavigationMenu.vue'
 import NavigationMenuLink from './NavigationMenuLink.vue'
 
-import { navMenuLinks } from './NavigationMenuLink.composible'
+import { navMenuLinks } from './NavigationMenuLink'
 
 defineComponent({
   name: 'Navigation',
@@ -14,7 +14,9 @@ defineComponent({
 
 <template>
   <header class="flex justify-center m-4">
-    <nav class="max-w-xl my-2 py-2 bg-[#192831] dark:bg-[#192831] flex flex-1 items-center justify-center absolute z-10 rounded-full shadow-lg" aria-label="Global">
+    <nav
+      class="max-w-xl my-2 py-2 bg-[#192831] dark:bg-[#192831] flex flex-1 items-center justify-center absolute z-10 rounded-full shadow-lg"
+      aria-label="Global">
       <template v-for="navMenuLink in navMenuLinks()">
         <NavigationMenu>
           <NavigationMenuLink :data="navMenuLink" />
--- src/components/navigation-menu/NavigationMenuLink.vue
+++ src/components/navigation-menu/NavigationMenuLink.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { defineComponent, ref } from 'vue';
-import type { NavlinkMenuLink } from './NavigationMenuLink.composible';
+import type { NavlinkMenuLink } from './NavigationMenuLink';
 
 defineComponent({
   name: 'NavigatioMenunLink',
@@ -14,5 +14,7 @@ const currentPath = ref(window.location.pathname)
 </script>
 
 <template>
-  <a :href="props.data.url" :class="props.data.url === currentPath ? 'bg-[#66667c] dark:bg-[#344551] text-white hover:text-white' : 'dark:bg-[#192831] hover:bg-[#494957] dark:hover:bg-[#353b41]'" class="px-3 py-2 text-[0.9rem] text-white dark:text-[#9FB9D0] font-semibold rounded-full">{{ props.data.name }}</a>
+  <a :href="props.data.url"
+    :class="props.data.url === currentPath ? 'bg-[#66667c] dark:bg-[#344551] text-white hover:text-white' : 'dark:bg-[#192831] hover:bg-[#494957] dark:hover:bg-[#353b41]'"
+    class="px-3 py-2 text-[0.9rem] text-white dark:text-[#9FB9D0] font-semibold rounded-full">{{ props.data.name }}</a>
 </template>
